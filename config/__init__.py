5099fdcdef33ced2dc445c10d76b7afd83e0d5db
song-junho <junho10000se@gmail.com>
2023-07-25 13:58:14 +0900

    FEAT: etf_info.json 파일 추가

--- config/__init__.py
+++ config/__init__.py
@@ -3,10 +3,15 @@ import json
 
 
 PATH_API_KEY = pathlib.Path(__file__).parents[1] / 'config' / f'api_key.json'
-PATH_DATA_INFO = pathlib.Path(__file__).parents[1] / 'config' / f'data_info.json'
+PATH_MACRO_INFO = pathlib.Path(__file__).parents[1] / 'config' / f'macro_info.json'
+PATH_ETF_INFO = pathlib.Path(__file__).parents[1] / 'config' / f'etf_info.json'
+
 
 with open(PATH_API_KEY, 'r', encoding='utf-8') as fp:
     API_KEY = json.load(fp)
 
-with open(PATH_DATA_INFO, 'r', encoding='utf-8') as fp:
-    DATA_INFO = json.load(fp)
+with open(PATH_MACRO_INFO, 'r', encoding='utf-8') as fp:
+    MACRO_INFO = json.load(fp)
+
+with open(PATH_ETF_INFO, 'r', encoding='utf-8') as fp:
+    ETF_INFO = json.load(fp)
