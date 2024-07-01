d460d7699a352cf6ff264777c328a051ca73899a
junho <junho10000se@gmail.com>
2024-07-01 09:40:51 +0900

    REFACTOR: [StockPack] 'KOSDSAQ GLOBAL' 데이터 추가

--- lib/stock_pack.py
+++ lib/stock_pack.py
@@ -13,8 +13,13 @@ def set_all_cmp_cd():
 
     if list_cmp_cd is None:
         df_krx = pd.read_sql_query('SELECT * FROM financial_data.krx_stock_info', db.conn)
-        list_cmp_cd = df_krx[df_krx["Market"].isin(["KOSPI", "KOSDAQ"])]["Symbol"].to_list()
+        list_cmp_cd = df_krx[df_krx["Market"].isin(["KOSPI", "KOSDAQ", "KOSDAQ GLOBAL"])]["Symbol"].to_list()
     else:
         list_cmp_cd = json.loads(list_cmp_cd)
+
+    list_cmp_cd.remove('107640')
+    list_cmp_cd.remove('464080')
+    list_cmp_cd.remove('160190')
+    list_cmp_cd.remove('295310')
 
     return list_cmp_cd
